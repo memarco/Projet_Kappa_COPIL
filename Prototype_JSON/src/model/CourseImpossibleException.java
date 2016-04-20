604747b52996a6b079c8ba43893a8dbdd2122b16
Trekiros <valentin.prevost.trekiros@gmail.com>
2016-04-20 17:49:42 +0200

    git rep cleanup
    Removed the R1 prototypes.
    
    Todo :
    -generate jars
    -apply the database changes to the VM
    -don't forget to changes server.properties (the DB url, login and
    password)

--- Prototype_JSON/src/model/CourseImpossibleException.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class CourseImpossibleException extends Exception {
-	public CourseImpossibleException(String message) {
-		super(message);
-	}
-
-	private static final long serialVersionUID = 1L;
-}
